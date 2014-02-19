29a0d5b9482382dacf2beff608c2b4e9938023b4
David Hoyt <dhoyt@guidewire.com>
2014-02-19 13:05:31 -0800

    Change client defaults.

--- src/test/scala/com/guidewire/tools/chronos/client/ClientDefaults.scala
+++ src/test/scala/com/guidewire/tools/chronos/client/ClientDefaults.scala
@@ -1,7 +1,7 @@
 package com.guidewire.tools.chronos.client
 
 object ClientDefaults {
-  val DEFAULT_HOST = "chronos.sandbox-master.guidewire.com"
+  val DEFAULT_HOST = "chronos.backyard.guidewire.com"
   val DEFAULT_PORT = DEFAULT_CHRONOS_PORT
   val DEFAULT_SECURE = false
 }
